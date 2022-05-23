bd60c1e1f1258e63b880a73a220847be572a695f
Olga Zhuravleva <onpol@mail.ru>
2022-05-23 13:22:33 +0300

    feat: 05-merge-styles

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,8 +1,8 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const dir1 = path.join(__dirname, "files");
-const dir2 = path.join(__dirname, "files-copy");
+const dir1 = path.join(__dirname, 'files');
+const dir2 = path.join(__dirname, 'files-copy');
 
 fs.mkdir(dir2, { recursive: true }, (err) => {
   if (err) throw err;
@@ -20,8 +20,9 @@ fs.readdir(dir2, (err, files) => {
 fs.readdir(dir1, (err, files) => {
   if (err) throw err;
   for (const file of files) {
-    fs.copyFile(path.join(dir1, file), path.join(dir2, file), (err) => {
+    fs.copyFile( path.join(dir1, file), path.join(dir2, file), (err) => {
       if (err) throw err;
-    });
+    }
+    );
   }
 });
