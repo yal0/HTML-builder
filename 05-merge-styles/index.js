ccb2d3228e84483409418d8f95f909ff2dee429c
Olga Zhuravleva <onpol@mail.ru>
2022-05-23 13:23:57 +0300

    fix: change quotes to '

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,20 +1,20 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 const output = fs.createWriteStream(
-  path.join(__dirname, "project-dist", "bundle.css")
+  path.join(__dirname, 'project-dist', 'bundle.css')
 );
-const styleDir = path.join(__dirname, "styles");
+const styleDir = path.join(__dirname, 'styles');
 
 fs.readdir(styleDir, { withFileTypes: true }, async (err, files) => {
   if (err) throw err;
   for await (const file of files) {
     if (
       file.isFile() &&
-      path.extname(path.join(styleDir, file.name)) === ".css"
+      path.extname(path.join(styleDir, file.name)) === '.css'
     ) {
       const pathToFile = path.join(styleDir, file.name);
-      const input = fs.createReadStream(pathToFile, "utf-8");
+      const input = fs.createReadStream(pathToFile, 'utf-8');
       input.pipe(output);
     }
   }
